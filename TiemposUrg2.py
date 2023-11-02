daa7c0de2cac37a24d7cb646f9f77cf07a775fe5
Victor Julio Gomez <41869547+vgomezc81972@users.noreply.github.com>
2023-11-02 12:33:47 -0500

    Add files via upload

--- TiemposUrg2.py
+++ TiemposUrg2.py
@@ -140,7 +140,7 @@ with st.container():
 
 with st.container():
     st.write("---")
-    st.header("Tiempo de Espera")
+    st.header("Historico de Tiempo de Espera de Atencion de Pacientes")
     left_column , right_column = st.columns(2)
 
     with left_column:
@@ -208,5 +208,77 @@ with st.container():
 
         # Muestra la figura en Streamlit
         st.pyplot(fig)
+
+with st.container():
+    st.write("---")
+    st.header("Predicción de Tiempo de Espera de Atencion de Pacientes")
+    left_column , right_column = st.columns(2)
+
+    with left_column:
+        st.header("DIA DE LA SEMANA")
+        st.write("Esta imagen muestra Por dias de la semana del Dia")
+    
+        # Ahora puedes acceder al día de la semana usando el atributo 'dayofweek'
+        df['day_of_week'] = df[mask]['FECHA_LLEGADA'].dt.dayofweek
+
+        promedio = df[mask]['Tiempo_Minutos_Total'].median()
+        df[mask].loc[df[mask]['Tiempo_Minutos_Total'] > 420, 'Tiempo_Minutos_Total'] = promedio
+        df[mask].loc[df[mask]['Tiempo_Minutos_Total'] < 0, 'Tiempo_Minutos_Total'] = promedio
+
+        # Calcula el promedio de las predicciones para cada día de la semana
+        average_predicted_minutes = df.groupby('day_of_week')['Tiempo_Minutos_Total'].mean()
+
+        # Establece los índices explícitamente
+        average_predicted_minutes.index = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo']
+
+        # Trazar el gráfico de barras
+        fig, ax = plt.subplots(figsize=(10, 6))
+        sns.barplot(x=average_predicted_minutes.index, y=average_predicted_minutes.values, palette='viridis', ax=ax)
+        ax.set_xlabel('Día de la Semana')
+        ax.set_ylabel('Promedio del Tiempo (minutos)')
+        ax.set_title('Promedio del Tiempo por Día de la Semana')
+
+        # Añade etiquetas a las barras
+        for i, bar in enumerate(ax.patches):
+            yval = bar.get_height()
+            xval = bar.get_x() + bar.get_width() / 2
+            ax.text(xval, yval, f"{round(yval, 2)}", ha='center', va='bottom')
+
+        # Muestra la figura en Streamlit
+        st.pyplot(fig)
+
+    with right_column:
+        st.header("HORAS DEL DIA")
+        st.write("Esta imagen muestra Por Horas del Dia")
+    
+        # Ahora puedes acceder al día de la semana usando el atributo 'dayofweek'
+        df['Hora_del_dia'] = df[mask]['FECHA_LLEGADA'].dt.hour
+
+        promedio = df[mask]['Tiempo_Minutos_Total'].median()
+        df[mask].loc[df[mask]['Tiempo_Minutos_Total'] > 420, 'Tiempo_Minutos_Total'] = promedio
+        df[mask].loc[df[mask]['Tiempo_Minutos_Total'] < 0, 'Tiempo_Minutos_Total'] = promedio
+
+        # Calcula el promedio de las predicciones para cada día de la semana
+        average_predicted_minutes = df.groupby('Hora_del_dia')['Tiempo_Minutos_Total'].mean()
+
+        # Establece los índices explícitamente
+        # average_predicted_minutes.index = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo']
+
+        # Trazar el gráfico de barras
+        fig, ax = plt.subplots(figsize=(10, 6))
+        sns.barplot(x=average_predicted_minutes.index, y=average_predicted_minutes.values, palette='viridis', ax=ax)
+        ax.set_xlabel('Hora del Dia')
+        ax.set_ylabel('Promedio del Tiempo (minutos)')
+        ax.set_title('Promedio del Tiempo en Horas')
+
+        # Añade etiquetas a las barras
+        for i, bar in enumerate(ax.patches):
+            yval = bar.get_height()
+            xval = bar.get_x() + bar.get_width() / 2
+            ax.text(xval, yval, f"{round(yval, 2)}", ha='center', va='bottom')
+
+        # Muestra la figura en Streamlit
+        st.pyplot(fig)
+
 
 # %%
